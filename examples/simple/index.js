260a5b75699656c898e5e7760e50b04283ccca05
Santosh <Santosh@sybite.com>
2017-02-22 19:51:47 +0530

    Update

---
+++ examples/simple/index.js
@@ -0,0 +1,12 @@
+/* @flow */
+import React from 'react'
+import ReactDom from 'react-dom'
+import TimeAgo from '../../src/index'
+
+ReactDom.render(
+  <div>
+    You opened this page {' '}
+    <TimeAgo date={Date.now()} />
+  </div>,
+  document.getElementById('app')
+)
